4ed22948aef8fa4a482b92438c55ffc3732e98c1
ashmunji <51296685+ashmunji@users.noreply.github.com>
2020-08-12 23:32:02 -0500

    Update io-example.js

--- src/components/io-example/io-example.js
+++ src/components/io-example/io-example.js
@@ -3,5 +3,11 @@ import IO from 'components/io';
 import Title from 'components/title';
 import { Container } from './io-example.css';
 
+// <IO> uses a render prop to pass down `isVisible` and `hasBeenVisible`.
+// In this example, we only care about `isVisible` and reset the styles
+// every time we scroll back up. Use `hasBeenVisible` to keep the styles
+// after scrolling back up and down again.
+const IOExample = () => (
+);
 
 export default IOExample;
