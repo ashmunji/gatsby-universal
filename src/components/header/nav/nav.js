0bf643280f655a58a49e6fe5fb845da83ee87479
ashmunji <51296685+ashmunji@users.noreply.github.com>
2020-08-12 22:55:33 -0500

    dddd

--- src/components/header/nav/nav.js
+++ src/components/header/nav/nav.js
@@ -9,7 +9,10 @@ const Nav = () => (
         <Link to="/about">About</Link>
       </li>
       <li>
-        <a href="https://github.com/fabe/gatsby-universal">GitHub</a>
+        <a href="https://github.com/fabe/gatsby-universal">Blog</a>
+      </li>
+      <li>
+        <a href="https://github.com/fabe/gatsby-universal">Shop</a>
       </li>
     </ul>
   </Container>
